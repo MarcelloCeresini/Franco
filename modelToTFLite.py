404fa163ae1eabfa8a04ce48c8b1d167cd18fc7e
MarcelloCeresini <mceresini.97@gmail.com>
2021-03-27 17:43:23 +0000

    modelToTFLite modificato

--- modelToTFLite.py
+++ modelToTFLite.py
@@ -1,24 +1,16 @@
 import os
 import tensorflow as tf
 import tensorflow_datasets as tfds
+from data_preprocessing import preprocess_dataset
 
-(ds_train_init, ds_test), ds_info = tfds.load(
-    'mnist',
-    split=['train', 'test'],
+ds_init = tfds.load(
+    'speech_commands',
+    split='train[:1%]',
     shuffle_files=True,
-    as_supervised=True,
-    with_info=True,
+    as_supervised=True
 )
 
-
-def normalize_img(image, label):
-    return tf.cast(image, tf.float32) / 255., label
-
-
-def representative_dataset():  # to QUANTIZE the result and the weights
-    for data in ds_train_init.map(normalize_img).batch(1).take(int(ds_info.splits['train'].num_examples/10)):
-        yield [data[0]]
-
+representative_dataset = preprocess_dataset(ds_init)
 
 converter = tf.lite.TFLiteConverter.from_saved_model("saved_model")
 converter.optimizations = [tf.lite.Optimize.DEFAULT]
