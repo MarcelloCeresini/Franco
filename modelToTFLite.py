c15fba5b9b282ac69e30d0a4a5a91c201fef8d7d
MarcelloCeresini <mceresini.97@gmail.com>
2021-03-28 11:42:49 +0200

    corrected file for audio use + addition of an iterator for representative dataset

--- modelToTFLite.py
+++ modelToTFLite.py
@@ -1,18 +1,32 @@
 import os
+import pathlib
+
+print()
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+print()
+
 import tensorflow as tf
 import tensorflow_datasets as tfds
-from data_preprocessing import preprocess_dataset
+from data_preprocessing import get_spectrogram
 
+data_dir = pathlib.Path('data/speech_commands')
 ds_init = tfds.load(
     'speech_commands',
-    split='train[:1%]',
+    split='train[:10%]',
     shuffle_files=True,
+    data_dir=data_dir,
     as_supervised=True
 )
 
-representative_dataset = preprocess_dataset(ds_init)
+AUTOTUNE = tf.data.AUTOTUNE
 
-converter = tf.lite.TFLiteConverter.from_saved_model("saved_model")
+
+def representative_dataset():  # it needs an iterator
+    for data in ds_init.map(get_spectrogram,  num_parallel_calls=AUTOTUNE).batch(1).take(100):
+        yield [data.astype(tf.float32)]
+
+
+converter = tf.lite.TFLiteConverter.from_saved_model("training/model")
 converter.optimizations = [tf.lite.Optimize.DEFAULT]
 converter.representative_dataset = representative_dataset
 converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
